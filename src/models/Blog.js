8c0f3c333ea87ec1c9a68f69865929432cbb8587
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2025-02-15 11:16:14 +0600

    init graphql api-v1.0.1

--- src/models/Blog.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const BlogSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-});
-
-module.exports = mongoose.model('Blog', BlogSchema);
